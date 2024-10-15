14dd7fdc42340777d0cc1fd2cba8c1e4560a802c
Mikhail Tamarin <mikhail.tamarin@flipdish.com>
2024-10-15 16:00:31 +0300

    Initial action added

--- scripts/TranslationsDownload.ps1
+++ scripts/TranslationsDownload.ps1
@@ -61,9 +61,15 @@ function DownloadLproj([String] $tmpFolder,[String] $lang,[String] $locoExportKe
     $pathStrings = "{0}\{1}\{2}" -f $tmpFolder, $folderName, $fileNameStrings
     $pathInfoPlist = "{0}\{1}\{2}" -f $tmpFolder, $folderName, $fileNameInfoPlist
 
-    "Downloading to $folderName..."
+    "Downloading $fileNameStrings and $fileNameInfoPlist to $folderName..."
     Invoke-WebRequest -Uri $urlStrings -OutFile $pathStrings
     Invoke-WebRequest -Uri $urlInfoPlist -OutFile $pathInfoPlist
+
+    "Removing Loco comments from $pathStrings..."
+    Get-Content $pathStrings | Where-Object { $_ -notmatch "^  Exported at" -and $_ -notmatch "^  Exported by" } | Set-Content $pathStrings
+
+    "Removing Loco comments from $pathInfoPlist..."
+    Get-Content $pathInfoPlist | Where-Object { $_ -notmatch "^  Exported at" -and $_ -notmatch "^  Exported by" } | Set-Content $pathInfoPlist
 }
 
 function DownloadResxs([String] $tmpFolder, [String] $locoExportKey, [String[]] $langs){    
