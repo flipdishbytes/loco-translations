034808cb6e81d142bf1400ed911ad4245411e95a
Mikhail Tamarin <mikhail.tamarin@flipdish.com>
2024-10-15 15:35:28 +0300

    Initial action added

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# loco-translations
+# Purpose
+
+This custom GitHub Action was created to integrate Loco Translations into your CI/CD pipeline. It allows you to download translations from `https://localise.biz` and create/update PR with translated files.
+
+# Github Action: Loco Translations `flipdishbytes/loco-translations@v1.0`
+
+To use this Datadog CI action, add it to your pipeline workflow YAML file. Here are examples of adding traces to the pipeline depending on your needs.
+
+### How it works?
+
+1. Checks if there is Pull Request opened with the `[LANG] Loco updates` title in the beginning.
+    1.1. If there is any it gets the latest one and reads base and target branches for this PR to reuse them.
+    1.2. If there is no any PR opened it will generate branch name based on the current data `loco_updates_{YYYY_MM_DD}` and will create PR in the end.
+2. Downloads translations from Loco based on `langs` array and `format`. Set `LOCOEXPORTKEY` secret in your GitHub Actions.
+3. Applies downloaded translations files (overwrite) to the `translationsFolder` folder.
+4. Checks if there are any changes based on `git status` and pushing them to the translations branch from the #1.
+5. Creates PR if there is no any yet.
+
+
+### How to use?
+
+```yaml
+name: GH Action workflow to download and apply Loco Translations
+
+on:
+  # allow to run manually
+  workflow_dispatch:
+
+permissions: 
+  id-token: write
+  contents: write
+  pull-requests: write
+
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Translations Loco
+        uses: flipdishbytes/loco-translations@v1.0
+        with:
+          locoExportKey: ${{ secrets.LOCOEXPORTKEY }} # https://localise.biz -> Project -> Developer tools -> Export key from your Loco project. Set LOCOEXPORTKEY secret in your GitHub Actions.
+          langs: 'en,bg,de,es,fr,it,nl,pl,pt,fi' #language tags should match Loco languages from the project
+          format: 'resx' 
+          # supported formats: 
+          #     resx (for .NET projects),
+          #     json (for Android and other ptojects using json language files),
+          #     lproj (for iOS projects).
+          translationsFolder: 'src/DotNET.Translations' #the folder where yout translation files are located.
+          GH_TOKEN: ${{ github.token }} # leave it like that of you don't need to assign PR to groups for review.
+          mainBranch: main # it's main by default. Set it to your repository default branch.
+```
--- scripts/TranslationsDownload.ps1
+++ scripts/TranslationsDownload.ps1
@@ -1,5 +1,5 @@
 ###Add environment variable: locoExportKey
-###Add environment variable: langs - 'en-GB,uk-UA' by default
+###Add environment variable: langs - 'en,uk-UA' as example
 ###Add environment variable: tmpFolder - 'tmp' by default
 ###Add environment variable: format
 ###Set working firectory to source code
@@ -35,10 +35,57 @@ function DownloadResx([String] $tmpFolder,[String] $lang,[String] $locoExportKey
     Get-Content $path | Where-Object { $_ -notmatch "^  Exported at" -and $_ -notmatch "^  Exported by" } | Set-Content $path
 }
 
+function DownloadJson([String] $tmpFolder,[String] $lang,[String] $locoExportKey){
+    $url = "https://localise.biz/api/export/locale/{0}.json?status=translated&key={1}" -f $lang, $locoExportKey
+
+    $fileName =  "{0}.json" -f $lang
+
+    $path = "{0}\{1}" -f $tmpFolder, $fileName
+
+    "Downloading to $path..."
+    Invoke-WebRequest -Uri $url -OutFile $path
+}
+
+function DownloadLproj([String] $tmpFolder,[String] $lang,[String] $locoExportKey){
+    $folderName = "{0}.lproj" -f $lang
+
+    if (!(Test-Path $folderName)) {
+        New-Item -ItemType Directory -Path $folderName
+    }
+
+    $fileNameStrings = "Localizable.strings"
+    $fileNameInfoPlist = "InfoPlist.strings"
+
+    $urlStrings = "https://localise.biz/api/export/locale/{0}.strings?status=translated&key={1}" -f $lang, $locoExportKey
+    $urlInfoPlist = "https://localise.biz/api/export/locale/{0}.plist?status=translated&key={1}" -f $lang, $locoExportKey
+
+
+    $pathStrings = "{0}\{1}" -f $tmpFolder, $fileNameStrings
+    $pathInfoPlist = "{0}\{1}" -f $tmpFolder, $fileNameInfoPlist
+
+    "Downloading to $folderName..."
+    Invoke-WebRequest -Uri $urlStrings -OutFile $pathStrings
+    Invoke-WebRequest -Uri $urlInfoPlist -OutFile $pathInfoPlist
+}
+
 function DownloadResxs([String] $tmpFolder, [String] $locoExportKey, [String[]] $langs){    
     Foreach ($lang in $langs)
     {
         DownloadResx $tmpFolder $lang $locoExportKey
+    }
+}
+
+function DownloadJsons([String] $tmpFolder, [String] $locoExportKey, [String[]] $langs){    
+    Foreach ($lang in $langs)
+    {
+        DownloadJson $tmpFolder $lang $locoExportKey
+    }
+}
+
+function DownloadLprojs([String] $tmpFolder, [String] $locoExportKey, [String[]] $langs){    
+    Foreach ($lang in $langs)
+    {
+        DownloadLproj $tmpFolder $lang $locoExportKey
     }
 }
 
@@ -56,8 +103,12 @@ switch ($format) {
     "json" {
         DownloadJsons $tmpFolder $env:locoExportKey $langs
     }
+    "lproj" {
+        DownloadLprojs $tmpFolder $env:locoExportKey $langs
+    }
     default {
         Write-Host "Unsupported format: $format"
+        Write-Host "Supported formats: resx, json, lproj"
         exit 1
     }
 }
